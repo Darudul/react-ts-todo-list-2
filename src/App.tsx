004f26db69ca252df62a14df51050a6f22b5e6e3
Md Darudul Islam <darudulislam@gmail.com>
2022-11-27 09:02:14 +0800

    ts-todo-2 completed

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,13 @@ const App: FC = () => {
     setTask("");
     setDeadline(0);
   };
+  const completeTask = (taskNameToDelete: String): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
 
   return (
     <div className="App">
@@ -46,7 +53,7 @@ const App: FC = () => {
       </div>
       <div className="todoList">
         {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task} />;
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })}
       </div>
     </div>
